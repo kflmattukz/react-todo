d55fcc4bc3765f62e3cec726d10f2ff149ee1e02
Sulkifli Asmunandar <kflmattukz@gmail.com>
2022-06-23 19:16:13 +0800

    structure project folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header () {
+    return (
+        <div className="text-center pt-5">
+            <h2 className="text-4xl font-thin text-gray-700 pt-3">Welcome to Todoapp - React</h2>
+            <p className="text-xl font-bold text-gray-600 pt-3">manage your todo app</p>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,12 @@
+function InputTodo () {
+    return (
+        <div className="pt-5 mx-auto">
+            <form action="">
+                <input type="text" name="todo-item" id="todo-item" />
+                <button type="submit">Add Todo</button>
+            </form>
+        </div>
+    )
+}
+
+export default InputTodo
