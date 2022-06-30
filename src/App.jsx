0d291d3b25a25d61893417650e7a43dd6b8ea734
Sulkifli Asmunandar <kflmattukz@gmail.com>
2022-06-30 17:12:28 +0800

    Rename components name 😉😊

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import Header from './components/Header'
-import InputTodo from './components/InputTodo'
-import Todolist from './components/Todolist'
+import Todo from './components/Todo'
 
 function App() {
-
-  let todos = [
-    {
-      id: 1,
-      task: "learn javascript",
-      completed: false
-    },
-    {
-      id: 2,
-      task: "learn react",
-      completed: false
-    },
-    {
-      id: 3,
-      task: "make a simple project app",
-      completed: false
-    }
-  ]
-
-  let todoList = todos.map( todo => {
-    return <Todolist key={todo.id} todo={todo}/>
-  })
+  
 
   return (
     <div className="App w-1/3 mx-auto">
-      <Header/>
-      <InputTodo />
-      {todoList}
+      <Todo/>
     </div>
   )
 }
