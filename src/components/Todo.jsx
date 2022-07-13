c9fc58c4d305a70d1468109f42623ffe2ca976d1
Sulkifli Asmunandar <kflmattukz@gmail.com>
2022-07-13 09:26:10 +0800

    update style - responsive

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,53 +1,12 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState} from 'react'
 import TodoForm from './TodoForm'
 import TodoInfo from './TodoInfo'
 import TodoList from './TodoList'
 
 function Todo() {
 
-  // const data = {
-  //   getId(id) {
-  //     return localStorage.key(id)
-  //   },
-  //   store(id, content) {
-  //     localStorage.setItem(id, JSON.stringify(content), null, 4)
-  //   },
-  //   getByIndex(index) {
-  //     return JSON.parse(localStorage.getItem(this.getId(index)))
-  //   },
-  //   getById(id) {
-  //     return JSON.parse(localStorage.getItem(id))
-  //   },
-  //   remove(id) {
-  //     localStorage.removeItem(id)
-  //   },
-  // }
 
   const [todos , setTodos] = useState([])
-
-  // useEffect(() => {
-  //   if (localStorage.length === 0) {
-  //     return
-  //   }
-  //   for (let i = 0 ; i < localStorage.length ; i++) {
-  //       todos.push(data.getByIndex(i))
-  //     console.log(localStorage)
-  //   }
-  //   return updateTodo
-  // },[])
-
-  // function loadTodo () {
-  //   let updateTodo = []
-  //   for (let i = 0 ; i < localStorage.length ; i++) {
-  //     const todo = data.getByIndex(i);
-  //     updateTodo.push(todo)
-  //   }
-  //   // console.log(updateTodo)
-  //   setTodos(updateTodo)
-  // }
-  // loadTodo()
-
-
 
   const addTodo = (todo) => {
     if (!todo.task && /^\s*$/.test(todo.task)) {
@@ -96,7 +55,7 @@ function Todo() {
   })
 
   return (
-    <div className='flex flex-col gap-5 w-1/3 mx-auto'>
+    <div className='flex flex-col gap-5 w-4/5 sm:w-2/3 md:w-3/5 lg:w-2/5 mx-auto'>
       <header className='text-center'>
         <h2 className='text-4xl font-thin text-gray-600'>Simple Todo App</h2>
         <p className='text-xl font-semibold text-gray-600'>Manage your todos here</p>
